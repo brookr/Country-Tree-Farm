d56b69b755abba9f8d1218d0fd0863f7638cdaa4
Brook Riggio <brooke.riggio@gmail.com>
2012-11-26 17:07:54 -0800

    App config for Heroku.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -27,10 +27,10 @@ module CountryTreeFarm
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # config.time_zone = "Central Time (US & Canada)"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
@@ -42,8 +42,8 @@ module CountryTreeFarm
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # Use SQL instead of Active Record"s schema dumper when creating the database.
+    # This is necessary if your schema can"t be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
@@ -56,7 +56,11 @@ module CountryTreeFarm
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+
+
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = "1.0"
   end
 end
