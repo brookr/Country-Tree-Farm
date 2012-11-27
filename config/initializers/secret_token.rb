2f9999d6ee43ea815799bf4d9318735377c478cb
Brook Riggio <brooke.riggio@gmail.com>
2012-11-26 17:02:36 -0800

    Converting to Rails 3 and Cedar.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountryTreeFarm::Application.config.secret_token = 'e77c69718e0d0d1c01b198fe07b216dafee04827887034088e67d59e6ac033e391da3f554a1f1ea2a55780f217ad01f56584ebf68fabd936834a7cd81cb774e6'
